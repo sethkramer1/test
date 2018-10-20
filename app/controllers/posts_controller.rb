29667a3d9b57a0d5bc0409017472bef647ea88f6
Seth Kramer <sethaepiuva@gmail.com>
2018-10-20 13:28:06 -0400

    updated index page design

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -69,6 +69,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :description, :url)
+      params.require(:post).permit(:title, :description, :url, :category)
     end
 end
